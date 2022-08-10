02492358d63497fec8797756f6bff1c974951c21
RadioWP <6474544@gmail.com>
2022-08-10 10:19:34 +0300

    feat: add a description of the solution to index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ module.exports = function reverse(n) {
   return +reversedInteger;
 
 }
+
+// The solution of the Reverse Int task:
+
+//1. Convert number to an absolute value using the Math.abs() method;
+//2. Convert absolute value to a string using the toString() method;
+//3. Split a string into an array of substrings using the split() method;
+//4. Reverse the order of the array elements using the reverse() method;
+//5. Combine all elements of the array into a new string using the join() method;
+//6. Convert the string to a number using the unary plus (+) operator;
