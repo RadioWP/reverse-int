48ac472c23198359734725723719988544e2b286
RadioWP <6474544@gmail.com>
2022-08-10 10:10:49 +0300

    feat: add task solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+  n = Math.abs(n);
+  let reversedInteger = n.toString().split('').reverse().join('');
+  return +reversedInteger;
+
 }
